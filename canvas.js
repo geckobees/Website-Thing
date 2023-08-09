937c2153920495ec47586f532f5fe0f1c40b82f6
geckobees <98201133+geckobees@users.noreply.github.com>
2023-08-08 23:58:16 -0400

    updates

--- canvas.js
+++ canvas.js
@@ -33,12 +33,13 @@ function Circle(x, y, dx, dy, radius) {
     this.y = y;
     this.dx = dx;
     this.dy = dy;
+    this.radius = radius
     this.minRadius = radius;
     
     this.draw = function() { // how to draw the shape
         c.beginPath();
         c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-        c.stroke();
+        c.fill();
         
             
     }
@@ -53,16 +54,15 @@ function Circle(x, y, dx, dy, radius) {
         }
         if (this.y + this.radius > innerHeight|| this.y - this.radius < 0){
             this.dy = -this.dy;
-            console.log('ow')
         }
         this.draw()
 
         
         //interactivity
-
+        
         if (mouse.x - this.x < 50 && mouse.x - this.x > -50 && mouse.y - this.y < 50 && mouse.y - this.y > -50 ){
-            if (this.radius <  this.minRadius){
-                this.radius += 6
+            if (this.radius < maxRadius){
+                this.radius += 2
             }
         }else if(this.radius > this.minRadius){
             this.radius -= 1
@@ -76,13 +76,12 @@ function Circle(x, y, dx, dy, radius) {
 var circleArray = [];
 
 for (var i = 0; i < 600; i++) {
-    var radius = Math.random(0.5, 1) * 15 + 1
+    var radius = Math.random() * 3 + 1
     var x = Math.random() * (window.innerWidth - radius * 2) + radius;
     var y = Math.random() * (window.innerHeight - radius * 2) + radius;
     var dx = (Math.random() - 0.5) * 3;
     var dy = (Math.random() - 0.5) * 3;
     circleArray.push(new Circle(x, y, dx, dy, radius))
-    console.log('draw');
 }
 
 
